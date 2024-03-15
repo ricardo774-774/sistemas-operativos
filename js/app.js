da2435defe06de51aed00f89b515715f9f57ef37
Ricardo Velez <ricardovelecal@gmail.com>
2024-03-15 00:00:12 -0600

    parte 4 lista

--- js/app.js
+++ js/app.js
@@ -2,13 +2,16 @@
 
 // Global Counter
 let globalCounter = 0;
+let globalInterval;
 
 // Execution Counter
 let executionCounter = 0;
-let executionInterval = 0;
+let executionIntervalId = 0;
+let executionTimeOut;
 
 // Ready counter
 let readyCounter = 0;
+let readyIntervalId = 0; 
 
 // Waiting counter
 let waitingCounter = 0;
@@ -96,7 +99,7 @@ let contenedor = document.getElementById("terminado");
 
 // DOMContentLoaded
 document.addEventListener('DOMContentLoaded', function () {
-    setInterval(incrementarContador, 1000);
+    globalInterval = setInterval(incrementarContador, 1000);
 });
 
 // Submit form 
@@ -253,9 +256,6 @@ const timeOfInitToFinish = (process) => {
 }
 
 const showFinished = (process) => {
-
-    let returnTime; // Tiempo total desde que el proceso llega hasta que termina.
-    let responseTime; // Tiempo transcurrido desde que llega hasta que es atendido por primera vez.
     
     // New stage of process
     process.stage = 'terminado';
@@ -303,6 +303,94 @@ const showFinished = (process) => {
     contenedor.appendChild(hr);
 }
 
+const showFinishedWithErr = (process) => {
+
+    clearInterval(readyIntervalId);
+    clearInterval(executionIntervalId);
+    clearTimeout(executionTimeOut);
+
+    showInExecution(listAllProcesses[Number(divExecution[0].textContent)]);
+    showListos(listAllProcesses[Number(divExecution[0].textContent)]);
+
+    // New stage of process
+    process.stage = 'terminado';
+    
+    // Calculate time data of finished
+    process.completionTime = getCurrentTime();
+
+    // Calculate time from start to finish of process
+    process.returnTime = timeOfInitToFinish(process);
+
+    // If not blocked, add normal service time
+    if (!process.serviceTime) {
+        process.serviceTime = process.estimatedTime;
+    } else {
+        // Add time in block
+        process.serviceTime = process.estimatedTime + process.estimatedTime;
+    }
+
+    // Order of process
+    process = {
+        id: process.id,
+        num1: process.num1,
+        num2: process.num2,
+        operation: process.operation,
+        result: 'ERROR',
+        arrivalTime: process.arrivalTime,
+        completionTime: process.completionTime,
+        estimatedTime: process.estimatedTime,
+        serviceTime: process.serviceTime,
+        waitTime: process.waitTime,
+        returnTime: process.returnTime
+    }
+    
+    // Show to user
+    for (let property in process) {
+        if (process.hasOwnProperty(property)) {
+            let div = document.createElement("div");
+            div.classList.add('col-sm-3', 'col-lg-12', 'text-center');
+            div.textContent = `${property}: ${process[property]}`;
+            contenedor.appendChild(div);
+        }
+    }
+    let hr = document.createElement("hr");
+    hr.classList.add('altura-2rem');
+    contenedor.appendChild(hr);
+}
+
+const createNewProcess = () => {
+    // Generar números aleatorios para el proceso
+    const number1 = Math.floor(Math.random() * 10) + 1;
+    const number2 = Math.floor(Math.random() * 10) + 1;
+
+    // Obtener una operación aleatoria
+    const operationIndex = Math.floor(Math.random() * operationI.length);
+    const operation = operationI[operationIndex];
+
+    // Obtener tiempo estimado aleatorio
+    const estimatedTime = Math.floor(Math.random() * (18 - 7 + 1)) + 7;
+
+    // Obtener hora actual
+    const currentTime = getCurrentTime();
+
+    // Crear el objeto del proceso
+    const newProcess = {
+        id: listAllProcesses.length + 1, // Incrementar el ID
+        num1: number1,
+        num2: number2,
+        operation: operation,
+        estimatedTime: estimatedTime,
+        arrivalTime: currentTime,
+        stage: 'nuevo'
+    };
+
+    // Añadir el proceso a la lista de procesos
+    listAllProcesses.push(newProcess);
+
+    updateNewProcessCountUpper();
+};
+
+
 const showListos = (process) => {
 
     process.stage = 'listo';
@@ -313,12 +401,12 @@ const showListos = (process) => {
     let counter;
     counter = Number(divExecution[5].textContent);
 
-    let interval = setInterval(() => {
+    readyIntervalId = setInterval(() => {
         counter = counter - 1;
         divListo[2].textContent = counter;
 
         if (counter == 0) {
-            clearInterval(interval);
+            clearInterval(readyIntervalId);
 
             // Show list of new processes
             updateNewProcessCount();
@@ -349,7 +437,10 @@ const showInExecution = (process) => {
     executionCounter = 0;
 
     // Start interval
-    executionInterval = setInterval(counterExecution, 1000);
+    executionIntervalId = setInterval(() => {
+        counterExecution();
+    }, 1000);
+
 
     // Show data
     divExecution[0].textContent = process?.id || '';
@@ -366,13 +457,13 @@ const showInExecution = (process) => {
 
 
     // When finish time
-    setTimeout( () => {
+    executionTimeOut =  setTimeout( () => {
 
         // Pass to finished process
         showFinished(process);
 
         // Stop execution counter
-        clearInterval(executionInterval);
+        clearInterval(executionIntervalId);
 
         // Clear data showed
         divExecution[0].textContent = '';
@@ -413,16 +504,91 @@ const getNextProcessInExe = () => {
         : { isExe: false, processExe: {} }
 }
 
+const seeAllProcesses = () => {
+    // Obtener el modal
+    const modal = document.getElementById("modal");
+
+    // Obtener el contenedor de procesos dentro del modal
+    const processContainer = document.getElementById("process-container");
+
+    // Limpiar contenido previo del contenedor de procesos
+    processContainer.innerHTML = '';
+
+    // Algo bloqueado?
+    let idBloqueado;
+    if (divBloqueados[0].textContent !== '') {
+        idBloqueado = Number(divBloqueados[0].textContent);
+    }
+
+    // Iterar sobre todos los procesos en listAllProcesses
+    listAllProcesses.forEach((process, index) => {
+
+        // Crear un elemento div para mostrar los detalles del proceso
+        const processDiv = document.createElement("div");
+        processDiv.classList.add("process-details");
+
+        if (process.id == idBloqueado) {
+            processDiv.innerHTML = `
+            <h4>Proceso ${index + 1}</h4>
+            <p>ID: ${process.id}</p>
+            <p>Operación: ${process.operation}</p>
+            <p>Número 1: ${process.num1}</p>
+            <p>Número 2: ${process.num2}</p>
+            <p>Tiempo estimado: ${process.estimatedTime}</p>
+            <p>Tiempo de llegada: ${process.arrivalTime}</p>
+            <p>Estado: Bloqueado</p>
+        `;
+        } else {
+            // Agregar los detalles del proceso al div
+        processDiv.innerHTML = `
+            <h4>Proceso ${index + 1}</h4>
+            <p>ID: ${process.id}</p>
+            <p>Operación: ${process.operation}</p>
+            <p>Número 1: ${process.num1}</p>
+            <p>Número 2: ${process.num2}</p>
+            <p>Tiempo estimado: ${process.estimatedTime}</p>
+            <p>Tiempo de llegada: ${process.arrivalTime}</p>
+            <p>Estado: ${process.stage}</p>
+        `;
+        }
+
+        // Agregar el div del proceso al contenedor
+        processContainer.appendChild(processDiv);
+    });
+
+    // Mostrar el modal
+    modal.style.display = "block";
+};
+
+document.addEventListener('DOMContentLoaded', function() {
+    const closeButton = document.querySelector('.close');
+    const modal = document.getElementById("modal");
+
+    closeButton.addEventListener('click', function() {
+        modal.style.display = "none";
+    });
+
+    // Cerrar el modal cuando se hace clic fuera del contenido del modal
+    window.onclick = function(event) {
+        if (event.target == modal) {
+            modal.style.display = "none";
+        }
+    };
+});
+
 // Keydown functions
 const manejarTecla = (event) =>  {
     switch(event.key) {
         case 'i':
-        case 'I':
-            moveProcessToBlocked();
+        case 'I': {
+            if (divBloqueados[0].textContent === '') {
+                moveProcessToBlocked();   
+            }
+        }
             break;
         case 'e':
         case 'E':
-            moveProcessToFinished();
+            showFinishedWithErr(listAllProcesses[divExecution[0].textContent-1]);
             break;
         case 'p':
         case 'P':
@@ -432,44 +598,46 @@ const manejarTecla = (event) =>  {
         case 'C':
             continueProgram();
             break;
+        case 'n':
+        case 'N':
+            createNewProcess();
+            break;
+        case 'b':
+        case 'B':
+            seeAllProcesses();
+            break;
     }
 }
 
 const moveProcessToBlocked = () => {
-    // Show data bloqued
-    divBloqueados[0].textContent =  divExecution[0].textContent;
-    divBloqueados[1].textContent =  10;
+    let counter = 10;
+    divBloqueados[0].textContent = divExecution[0].textContent;
+    divBloqueados[1].textContent = counter;
 
-    let counter = 10; // Counter
-    let position = divBloqueados[0].textContent;
+    clearInterval(readyIntervalId);
+    clearInterval(executionIntervalId);
+    clearTimeout(executionTimeOut);
 
-    // Change stage
-    listAllProcesses[position - 1].stage = 'nuevo';
+    showInExecution(listAllProcesses[Number(divBloqueados[0].textContent)]);
+    showListos(listAllProcesses[Number(divBloqueados[0].textContent) + 1]);
 
-    // Add blocked to the end
-    listAllProcesses.push(listAllProcesses[position - 1]);
-
-    // Remove blocked process from the list 
-    listAllProcesses.splice(position - 1 ,1);
-
-    // Add 1 to counter new process
     updateNewProcessCountUpper();
 
-    clearInterval(executionInterval);
-
-    showInExecution(listAllProcesses[position - 1]);
-    showListos(listAllProcesses[position - 2]);
-
-    // listAllProcesses.forEach((process) => {
-    //     console.log(process.id, process.stage);
-    // });
+    listAllProcesses[divBloqueados[0].textContent - 1].serviceTime = 1;
+    listAllProcesses[divBloqueados[0].textContent - 1].stage = 'nuevo';
+    listAllProcesses.push(listAllProcesses[divBloqueados[0].textContent - 1]);
+    listAllProcesses.splice(divBloqueados[0].textContent - 1,1);
 
     let interval = setInterval(() => {
+
+        counterWaiting();
+
         counter = counter - 1;
         divBloqueados[1].textContent = counter;
 
         if (counter == 0) {
-            // listAllProcesses[position - 1].stage = 'nuevo';
+            divBloqueados[0].textContent = '';
+            divBloqueados[1].textContent = '';
             clearInterval(interval);
         }
 
@@ -478,11 +646,97 @@ const moveProcessToBlocked = () => {
 
 
 const pauseProgram = () => {
-    
+    // Intervalo de contador 
+    clearInterval(globalInterval);
+
+    // Intervalos de procesos
+    clearInterval(readyIntervalId);
+    clearInterval(executionIntervalId);
+    clearTimeout(executionTimeOut);
 }
 
 const continueProgram = () => {
-    
+    // Reactivar el intervalo global
+    globalInterval = setInterval(incrementarContador, 1000);
+
+    // Reactivar el intervalo de procesos en listos
+    if (divListo[2].textContent !== "") {
+        readyIntervalId = setInterval(() => {
+            let counter = Number(divListo[2].textContent);
+            counter = counter - 1;
+            divListo[2].textContent = counter;
+
+            if (counter == 0) {
+                clearInterval(readyIntervalId);
+
+                // Mostrar lista de nuevos procesos
+                updateNewProcessCount();
+
+                divListo[0].textContent = "";
+                divListo[1].textContent = "";
+                divListo[2].textContent = "";
+
+                // Obtener y mostrar nuevo proceso en ejecución
+                let { isReady, processReady } = getNextProcessReady();
+                if (isReady == true) {
+                    showListos(processReady);
+                }
+            }
+        }, 1000);
+    }
+
+    // Reactivar el intervalo de ejecución de procesos
+    if (divExecution[4].textContent !== "") {
+        executionIntervalId = setInterval(() => {
+            counterExecution();
+        }, 1000);
+    }
+
+    // Reactivar el tiempo de espera para los procesos bloqueados
+    if (divBloqueados[1].textContent !== "") {
+        let counter = Number(divBloqueados[1].textContent);
+        let interval = setInterval(() => {
+            counter = counter - 1;
+            divBloqueados[1].textContent = counter;
+
+            if (counter == 0) {
+                divBloqueados[0].textContent = '';
+                divBloqueados[1].textContent = '';
+                clearInterval(interval);
+            }
+        }, 1000);
+    }
+
+    // Reactivar el tiempo de ejecución de los procesos
+    if (divExecution[5].textContent !== "") {
+        let estimatedTime = Number(divExecution[5].textContent);
+        estimatedTime = estimatedTime - 1;
+        executionTimeOut = setTimeout(() => {
+            // Obtener proceso en ejecución
+            let currentProcessId = divExecution[0].textContent;
+            let currentProcess = listAllProcesses[currentProcessId - 1];
+
+            // Pasar el proceso a terminado
+            showFinished(currentProcess);
+
+            // Detener contador de ejecución
+            clearInterval(executionIntervalId);
+
+            // Limpiar datos mostrados
+            divExecution[0].textContent = '';
+            divExecution[1].textContent = '';
+            divExecution[2].textContent = '';
+            divExecution[3].textContent = '';
+            divExecution[4].textContent = '';
+            divExecution[5].textContent = '';
+
+            // Obtener y mostrar nuevo proceso en ejecución
+            let { isExe, processExe } = getNextProcessInExe();
+            if (isExe == true) {
+                showInExecution(processExe);
+            }
+        }, estimatedTime * 1000);
+    }
 }
 
 ////////// Update new process span
